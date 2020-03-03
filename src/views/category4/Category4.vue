ca0afceb247b30915fe6f2b51366518645286ab8
beahyuk <xueqing-950822@163.com>
2020-03-03 18:22:01 +0800

    git commit

--- src/views/category4/Category4.vue
+++ src/views/category4/Category4.vue
@@ -1,11 +1,20 @@
 <template>
-    
+<CatePage :projectName="itemName4"></CatePage>
 </template>
 
 <script>
-  export default {
-    name: "Category4"
+import CatePage from "components/contents/CatePage";
+export default {
+  name: "Category4",
+  components: {
+    CatePage
+  },
+  data() {
+    return {
+      itemName4: ["正在执行", "任务执行", "回归测试" ],
+    }
   }
+}
 </script>
 
 <style scoped>
